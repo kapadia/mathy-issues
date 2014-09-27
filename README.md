48709fbd96614ce57e9fcd6bc0d0c55a85321e0d
Amit Kapadia <akapad@gmail.com>
2014-09-27 11:21:56 -0700

    use the label attr since it is whitelisted

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ mathy-issues
 ============
 
 Chrome Extension for rendering LaTeX within GitHub issues.
+
+Use the following markup to render LaTeX:
+
+    <p label="tex">[tex goes here]</p>
+    
+    // example
+    <p label="tex">c = \sqrt(a^2 + b^2)</p>
--- content_script.js
+++ content_script.js
@@ -1,7 +1,7 @@
 
 (function() {
   
-  var elems = document.querySelectorAll("div.comment-body p");
+  var elems = document.querySelectorAll("div.comment-body p[label='tex']");
   for (var i = 0; i < elems.length; i += 1) {
     var el = elems[i];
     katex.render(el.textContent, el);
