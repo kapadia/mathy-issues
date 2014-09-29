407e4447f0d178785eecacb0f12d2ffce55c0a5d
Amit Kapadia <akapad@gmail.com>
2014-09-28 20:02:11 -0700

    use background process

--- content_script.js
+++ content_script.js
@@ -2,9 +2,10 @@
 (function() {
   
   var elems = document.querySelectorAll("div.comment-body p[label='tex']");
-  console.log("HERE", elems);
+  
   for (var i = 0; i < elems.length; i += 1) {
     var el = elems[i];
+    console.log(el);
     katex.render(el.textContent, el);
   }
   
